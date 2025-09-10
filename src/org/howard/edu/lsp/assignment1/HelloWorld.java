fa89a12e431c3842073a8370e78b4cef64670d50
Mahlet Zegeye <mahlet@Mahlets-MacBook-Pro.local>
2025-09-10 02:25:22 -0400

    Initial commit for assignment 1

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: Mahlet Zegeye
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!!");
+    }
+}
